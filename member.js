2424a711ca76ccd52df8d1587c3f316340ce3251
Gaurang Agarwal <gaurangagarwal10@gmail.com>
2024-08-18 00:35:27 +0530

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    return {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "React", "Node"],
+        address: {
+            city: "New York",
+            country: "USA"
+        }
+    }
+}
